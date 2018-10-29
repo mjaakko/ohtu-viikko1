66919fee0ab2e3e02d7b2aa79cac897cbc32ca96
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-10-29 17:58:59 +0200

    Add NhlStatistics testing

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author jaakko
+ */
+public class StatisticsTest {
+ 
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }  
+    
+    @Test
+    public void testSearchNotNull() {
+        assertNotNull(stats.search("Semenko"));
+    }
+    
+    @Test
+    public void testSearchNull() {
+        assertNull(stats.search("Testi"));
+    }
+    
+    @Test
+    public void testTeam() {
+        assertEquals(3, stats.team("EDM").size());
+    }
+    
+    @Test
+    public void testTopScorers() {
+        assertEquals(1, stats.topScorers(0).size());
+    }
+}
