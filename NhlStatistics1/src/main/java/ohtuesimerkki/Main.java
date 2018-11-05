dbaeac45d156fd2ab8b007aed4f3f7106440e212
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-11-05 17:00:50 +0200

    Do not use magic number

--- NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
+++ NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -3,6 +3,8 @@ package ohtuesimerkki;
 import java.util.Collections;
 
 public class Main {
+    private static final int MAX_PLAYERS = 10;
+    
     public static void main(String[] args) {
         Reader reader = new PlayerReader("http://nhlstatistics.herokuapp.com/players.txt");
         Statistics stats = new Statistics(reader);
@@ -15,7 +17,7 @@ public class Main {
         System.out.println("");
         
         System.out.println("Top scorers");
-        for (Player player : stats.topScorers(10) ) {
+        for (Player player : stats.topScorers(MAX_PLAYERS) ) {
             System.out.println( player );
         }        
     }
